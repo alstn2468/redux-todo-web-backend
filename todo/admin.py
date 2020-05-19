404ecf1b0a590746735f0bdf89dfaaeac0a3b249
Kimminsu <alstn2468_@naver.com>
2020-05-19 09:31:22 +0900

    [#3] Create AbstractTimeStamp, Todo model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from todo.models import Todo
 
-# Register your models here.
+admin.site.register(Todo)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class AbstractTimeStamp(models.Model):
+    """Abstract TimeStamp Model
+    Inherit:
+        Model
+    Fields:
+        created_at : DateTimeField (UnEditable)
+        updated_at : DateTimeField (Editable)
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Todo(AbstractTimeStamp):
+    """Todo Model
+    Inherit:
+        AbstractTimeStamp
+    Fields:
+        text : CharField
+        is_completed : BooleanField
+    """
+
+    text = models.CharField(max_length=100)
+    is_completed = models.BooleanField(default=False)
