3013f5c10095880340d4acebb6392cbc6a13b466
Kimminsu <alstn2468_@naver.com>
2020-05-28 12:26:23 +0900

    [#40] Add try-except block for return unauthorized response

--- user/middleware.py
+++ user/middleware.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.core.exceptions import PermissionDenied
 from user.utils.jwt import decode_jwt
 from http import HTTPStatus
 
@@ -17,32 +18,35 @@ class JsonWebTokenMiddleWare(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path != "/signup" and request.path != "/login":
-            # Except signup and login
-            headers = request.headers
-            # Get Authorization header or None
-            access_token = headers.get("Authorization", None)
+        try:
+            if request.path != "/signup" and request.path != "/login":
+                # Except signup and login
+                headers = request.headers
+                # Get Authorization header or None
+                access_token = headers.get("Authorization", None)
 
-            # If access_token isn't exist
-            if not access_token:
-                return JsonResponse(
-                    {"error": "Authorization Error"}, status=HTTPStatus.UNAUTHORIZED
-                )
+                # If access_token isn't exist
+                if not access_token:
+                    raise PermissionDenied()
 
-            # Decode JWT token
-            payload = decode_jwt(access_token)
+                # Decode JWT token
+                payload = decode_jwt(access_token)
 
-            # Get user from decoded jwt payload
-            username = payload.get("user", None)
+                # Get user from decoded jwt payload
+                username = payload.get("user", None)
 
-            # If username is exist
-            if username:
-                # Get user object using username
-                User.objects.get(username=username)
+                # If username is exist
+                if username:
+                    # Get user object using username
+                    User.objects.get(username=username)
 
-        response = self.get_response(request)
+            response = self.get_response(request)
 
-        # SignUp or Login
-        # Create access token from request payload
+            # SignUp or Login
+            # Create access token from request payload
 
-        return response
+            return response
+        except (PermissionDenied, User.DoesNotExist):
+            return JsonResponse(
+                {"error": "Authorization Error"}, status=HTTPStatus.UNAUTHORIZED
+            )
