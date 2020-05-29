b0a0cd70ce336894fe3361569c5acdd2088c515f
Kimminsu <alstn2468_@naver.com>
2020-05-29 09:46:58 +0900

    [#46] Implement login_view logic

--- user/tests/test_views.py
+++ user/tests/test_views.py
@@ -1,15 +1,29 @@
 from django.test import TestCase
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.contrib.auth.models import User
+from user.utils.jwt import decode_jwt
 from http import HTTPStatus
 
 
 class UserViewTest(TestCase):
-    def test_login_view(self):
-        """User application login_view post method test
-        Check login_view return JsonResponse with access_token
+    def setUp(self):
+        """Run only once when running UserViewTest
+        Test User Object Fields :
+            id       : 1
+            username : test
+            password : 123456
         """
-        response = self.client.post("/login")
+        User.objects.create_user(username="test", password="123456")
+
+    def test_login_view_post_method_success(self):
+        """User application login_view post method without user test
+        Check login_view return JsonResponse with bad request error
+        """
+        response = self.client.post(
+            "/login",
+            data={"user": "test", "password": "123456"},
+            content_type="application/json",
+        )
         self.assertIsInstance(response, JsonResponse)
         self.assertEqual(HTTPStatus.OK, response.status_code)
 
@@ -17,9 +31,102 @@ class UserViewTest(TestCase):
 
         self.assertIn("access_token", json_response.keys())
 
-        data = json_response["access_token"]
+        access_token = json_response["access_token"]
 
-        self.assertEqual("test", data)
+        self.assertIn("user", decode_jwt(access_token).keys())
+        self.assertEqual("test", decode_jwt(access_token)["user"])
+
+    def test_login_view_post_method_nonexist_user(self):
+        """User application login_view post method nonexist user test
+        Check login_view return JsonResponse with bad request error
+        """
+        response = self.client.post(
+            "/login",
+            data={"user": "test2", "password": "123456"},
+            content_type="application/json",
+        )
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
+
+        json_response = response.json()
+
+        self.assertIn("error", json_response.keys())
+
+        data = json_response["error"]
+
+        self.assertEqual("An error has occurred. Please try again.", data)
+
+    def test_login_view_post_method_wrong_password(self):
+        """User application login_view post method wrong password test
+        Check login_view return JsonResponse with bad request error
+        """
+        response = self.client.post(
+            "/login",
+            data={"user": "test", "password": "12345678"},
+            content_type="application/json",
+        )
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
+
+        json_response = response.json()
+
+        self.assertIn("error", json_response.keys())
+
+        data = json_response["error"]
+
+        self.assertEqual("An error has occurred. Please try again.", data)
+
+    def test_login_view_post_method_without_user(self):
+        """User application login_view post method without user test
+        Check login_view return JsonResponse with bad request error
+        """
+        response = self.client.post(
+            "/login", data={"password": "123456"}, content_type="application/json"
+        )
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
+
+        json_response = response.json()
+
+        self.assertIn("error", json_response.keys())
+
+        data = json_response["error"]
+
+        self.assertEqual("An error has occurred. Please try again.", data)
+
+    def test_login_view_post_method_without_password(self):
+        """User application login_view post method without password test
+        Check login_view return JsonResponse with bad request error
+        """
+        response = self.client.post(
+            "/login", data={"user": "test"}, content_type="application/json"
+        )
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
+
+        json_response = response.json()
+
+        self.assertIn("error", json_response.keys())
+
+        data = json_response["error"]
+
+        self.assertEqual("An error has occurred. Please try again.", data)
+
+    def test_login_view_post_method_witout_body(self):
+        """User application login_view post method without body test
+        Check login_view return JsonResponse with bad request error
+        """
+        response = self.client.post("/login")
+        self.assertIsInstance(response, JsonResponse)
+        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
+
+        json_response = response.json()
+
+        self.assertIn("error", json_response.keys())
+
+        data = json_response["error"]
+
+        self.assertEqual("An error has occurred. Please try again.", data)
 
     def test_login_view_except_post_method(self):
         """User application login_view another method test
--- user/views.py
+++ user/views.py
@@ -2,7 +2,18 @@ from django.http import JsonResponse, HttpResponseNotAllowed
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from user.utils.jwt import encode_jwt, decode_jwt
+from datetime import datetime, timedelta
+from json import loads
 from http import HTTPStatus
+
+
+def generate_access_token(username):
+    iat = datetime.now()
+    exp = iat + timedelta(days=7)
+
+    data = {"iat": iat.timestamp(), "exp": exp.timestamp(), "user": username}
+
+    return encode_jwt(data)
 
 
 @csrf_exempt
@@ -10,11 +21,29 @@ def login_view(request):
     data = {}
     status = HTTPStatus.OK
 
-    if request.method == "POST":
-        data["access_token"] = "test"
+    try:
+        if request.method == "POST":
+            json_body = loads(request.body)
 
-    else:
-        return HttpResponseNotAllowed(["POST"])
+            username = json_body.get("user", None)
+            password = json_body.get("password", None)
+
+            if not username or not password:
+                raise ValueError()
+
+            user = User.objects.get(username=username)
+
+            if not user.check_password(password):
+                raise ValueError()
+
+            data["access_token"] = generate_access_token(username)
+
+        else:
+            return HttpResponseNotAllowed(["POST"])
+
+    except (ValueError, User.DoesNotExist):
+        data["error"] = "An error has occurred. Please try again."
+        status = HTTPStatus.BAD_REQUEST
 
     return JsonResponse(data, status=status)
 
